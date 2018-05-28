4138a913281842d52a9298a11593a4cd9718ef06
JiaruiMi <mjrpumc@outlook.com>
2018-05-28 15:24:39 +0200

    first commit

---
+++ simpleSingleCell_template.R
@@ -0,0 +1,6 @@
+#==============================================================================================
+#
+#                      Analyzing single-cell RNA-seq data containing UMI counts
+#
+#==============================================================================================
+setwd('/Users/mijiarui/Nature_Biotechnology_Paper/simpleSingleCell')
